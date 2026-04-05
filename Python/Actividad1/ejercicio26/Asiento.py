6278b9331c77c20e81b932332f0226efdf34bcee
Jonathan Tobón <jtobonc@unicartagena.edu.co>
2026-04-04 21:53:26 -0500

    feat(python): actualizacion de informacion para la explicacion del ejercicio

--- Python/Actividad1/ejercicio26/Asiento.py
+++ Python/Actividad1/ejercicio26/Asiento.py
@@ -1,11 +1,14 @@
+#con esto converti la clase en algo tipo record
 from dataclasses import dataclass
 
 @dataclass
+#se crea la clase ya con los constructores
 class Asiento:
     fila: int
     columna: int
     reservado: bool = False
 
+    #metodos necesarios
     def reservar(self):
         self.reservado = True
 
@@ -15,18 +18,18 @@ class Asiento:
     def mostrar(self):
         return "[X]" if self.reservado else "[ ]"
 
-
+#creo la clase Sala con las matrices y parametros necesarios
 class Sala:
     def __init__(self, filas, columnas):
         self.asientos = [[Asiento(i, j) for j in range(columnas)] for i in range(filas)]
-
+    #hago el recorrido con el for para que me muestre la matriz
     def mostrar(self):
         for fila in self.asientos:
             print("".join(a.mostrar() for a in fila))
-
+    #la reserva
     def reservar(self, f, c):
         self.asientos[f][c].reservar()
-
+    #la liberacion
     def liberar(self, f, c):
         self.asientos[f][c].liberar()
 
