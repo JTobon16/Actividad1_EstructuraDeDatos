d4af700207732ccffcb8e6e7f323858283af4bc9
Jonathan Tobón <jtobonc@unicartagena.edu.co>
2026-04-04 21:49:23 -0500

    feat(java): actualizo informacion para poder explicar el proyecto

--- Java/Actividad1/src/ejercicio26/asiento.java
+++ Java/Actividad1/src/ejercicio26/asiento.java
@@ -2,16 +2,17 @@ package ejercicio26;
 
 
 public class asiento {
+    //aqui defini el objeto del ejercicio, aqui no use matrices sino directamente un objeto dentro
     private int fila;
     private int columna;
     private boolean reservado;
-
+    //hice el contructor de nuestro objeto
     public asiento(int fila, int columna) {
         this.fila = fila;
         this.columna = columna;
         this.reservado = false;
     }
-
+    //cree mis dos metodos necesarios, el reservar y liberar
     public void reservar() {
         reservado = true;
     }
@@ -23,7 +24,7 @@ public class asiento {
     public boolean isReservado() {
         return reservado;
     }
-
+    //aqui nuestro mostrar, la parte visual al ejecutar
     public String mostrar() {
         return reservado ? "[X]" : "[ ]";
     }
--- Java/Actividad1/src/ejercicio26/sala.java
+++ Java/Actividad1/src/ejercicio26/sala.java
@@ -1,18 +1,20 @@
 package ejercicio26;
 
 public class sala {
+    //aqui creo mi matris del objeto
     private asiento[][] asientos;
-
+    //constructor, creo la matriz vacia
     public sala(int filas, int columnas) {
         asientos = new asiento[filas][columnas];
 
+        //el respectivo recorrido de la matriz
         for (int i = 0; i < filas; i++) {
             for (int j = 0; j < columnas; j++) {
                 asientos[i][j] = new asiento(i, j);
             }
         }
     }
-
+    // el mostrar sala, use el for each
     public void mostrarSala() {
         for (asiento[] fila : asientos) {
             for (asiento a : fila) {
@@ -21,7 +23,7 @@ public class sala {
             System.out.println();
         }
     }
-
+    //el reservar y liberar, llamo los metodos creado en mi objeto
     public void reservar(int fila, int columna) {
         asientos[fila][columna].reservar();
     }
